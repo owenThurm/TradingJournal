8626338898f7636f53590277f3f1eb6ff7f1b69c
owenThurm <owen.p.thurm@gmail.com>
2020-12-07 16:38:18 -0500

    Updated Settings to a menu format

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Menu, Row } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Deposit } from './Deposit';
+import { Switch, Link } from 'react-router-dom';
+import ProtectedRoute from '../ProtectedRoute';
+import { General } from './General';
 
 const { SubMenu } = Menu;
 
@@ -17,21 +21,34 @@ class Settings extends React.Component {
   render() {
     return(
       <div style={{ margin: 20 }}>
-        <Menu
-        onClick={this.handleClick}
-        style={{ width: 256, backgroundColor: 'white' }}
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
-        mode="inline"
-        >
-          <SubMenu title='Account Balance'>
-            <Menu.Item onClick={this.clicked}>Deposit Balance</Menu.Item>
-            <Menu.Item>Withdraw Balance</Menu.Item>
-          </SubMenu>
-          <SubMenu title='Login Information'>
-            <Menu.Item>Reset Password</Menu.Item>
-          </SubMenu>
-        </Menu>
+        <Row>
+          <Menu
+          onClick={this.handleClick}
+          style={{ width: 256, backgroundColor: 'white' }}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          >
+            <Menu.Item>
+              <Link to='/settings'>General Settings</Link>
+            </Menu.Item>
+            <SubMenu title='Account Balance'>
+              <Menu.Item>
+                <Link to='/settings/deposit'>
+                  Deposit Balance
+                </Link>
+              </Menu.Item>
+              <Menu.Item>Withdraw Balance</Menu.Item>
+            </SubMenu>
+            <SubMenu title='Login Information'>
+              <Menu.Item>Reset Password</Menu.Item>
+            </SubMenu>
+          </Menu>
+          <Switch>
+            <ProtectedRoute exact path="/settings" component={General} />
+            <ProtectedRoute exact path='/settings/deposit' component={Deposit} />
+          </Switch>
+        </Row>
       </div>
     );
   }

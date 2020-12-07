37720fc0a020a71a1f1da691cb75c9a3266946de
owenThurm <owen.p.thurm@gmail.com>
2020-12-07 16:37:06 -0500

    Added dummy components for General and Deposit settings

---
+++ src/Settings/Deposit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Deposit = props => {
+
+  console.log('component rendered');
+
+  return(
+    <div>deposit here</div>
+  )
+}
---
+++ src/Settings/General.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const General = props => {
+  return (
+    <div>General</div>
+  )
+}

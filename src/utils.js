f0768e983420a9e719e7299450bb710f0130563f
owenThurm <owen.p.thurm@gmail.com>
2020-12-08 14:36:06 -0500

    added isNumber validator to utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 //Maps dates to integers
-function mapDate(date) {
+export function mapDate(date) {
   var dateList = date.slice(0,10).split('-');
   return dateList[0] * 365 + dateList[1] * 30 + dateList[2];
 }
 
-export default mapDate;
+export const isNumber = (rule, value) => {
+  if(isNaN(value)) return Promise.reject();
+  else return Promise.resolve();
+}

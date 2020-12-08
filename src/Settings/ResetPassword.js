6ca227d7217d6e66337d8524e2cde444ff63e72b
owenThurm <owen.p.thurm@gmail.com>
2020-12-07 21:06:37 -0500

    basic inputs for settings

--- src/Settings/ResetPassword.js
+++ src/Settings/ResetPassword.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Input, Button } from 'antd';
 
 
 export const ResetPassword = props => {
   return (
     <Card style={{width: 1139, height: 550}}>
       Reset Password
+      <Input placeholder='New Password' />
+      <Button>Set Password</Button>
     </Card>
   )
 }
--- src/Settings/Withdraw.js
+++ src/Settings/Withdraw.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Button, Input } from 'antd';
 
 
 export const Withdraw = props => {
   return (
     <Card style={{width: 1139, height: 550}}>
       Withdraw
+      <Input placeholder='Withdraw Balance' />
+      <Button>Withdraw</Button>
     </Card>
-  )
+  );
 }

25f6c986aa68a791a2ef9f8ba64f353c659d825b
owenThurm <owen.p.thurm@gmail.com>
2020-12-08 14:38:36 -0500

    Implemented Form Layout

--- src/Settings/Withdraw.js
+++ src/Settings/Withdraw.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Card, Button, Input } from 'antd';
+import { Card, Button, Input, Form } from 'antd';
+import { isNumber } from '../utils';
 
 
 export const Withdraw = props => {
+
+  const onFinish = values => {
+    console.log('success', values);
+  }
+
   return (
     <Card style={{width: 1139, height: 550}}>
       Withdraw
-      <Input placeholder='Withdraw Balance' />
-      <Button>Withdraw</Button>
+      <Form onFinish={onFinish}>
+        <Form.Item name='Deposit' rules={[{
+          required: true,
+          message: 'Enter a Withdraw Value',
+          validator: isNumber
+        }]}>
+          <Input placeholder='Withdraw Value'/>
+        </Form.Item>
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>Withdraw</Button>
+        </Form.Item>
+      </Form>
     </Card>
   );
 }
